63ec3c6aaf75ba0b00c9d65d2dbd229002ee274a
Gordon Doskas <gordon2012@users.noreply.github.com>
2020-02-20 00:38:01 -0700

    Part 5
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- WorldComplete.gd
+++ WorldComplete.gd
@@ -7,4 +7,5 @@ func _physics_process(_delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Player":
+# warning-ignore:return_value_discarded
 			get_tree().change_scene(next_world)
